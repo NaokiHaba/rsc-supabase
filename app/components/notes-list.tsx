586e960c5b3c38608e563ca3693f22d2dea5b583
Naoki Haba <naosprintrunner6385@gmail.com>
2023-09-17 23:25:59 +0900

    feat: dynamic component notes

--- app/components/notes-list.tsx
+++ app/components/notes-list.tsx
@@ -1,63 +1,50 @@
-"use client";
-
 import { Database } from "@/database.types"
-import useSWR from "swr"
-import { format } from 'date-fns'
-import Loading from "@/app/loading"
+import { format } from "util"
 
 type Note = Database['public']['Tables']['notes']['Row']
 
-async function fetcher(url: string) {
-    const res = await fetch(url, {
+async function fetchNotes() {
+    await new Promise(resolve => setTimeout(resolve, 2000))
+    const res=await fetch(`${process.env.API_URL}/rest/v1/notes?select=*`, {
         headers: new Headers({
-            apikey: process.env.NEXT_PUBLIC_API_KEY as string
+            apikey: process.env.API_KEY as string
         }),
+
+
+        // force-cache: キャッシュがある場合でも、リクエストを送信せずにキャッシュからレスポンスを取得します(default)
+        // no-cache: キャッシュを使用しないで、常にサーバーからレスポンスを取得します。
+        // no-store: レスポンスをキャッシュしないで、常にサーバーからレスポンスを取得します。
+        // only-if-cached: キャッシュがある場合にのみ、キャッシュからレスポンスを取得します。
+        // 応答がキャッシュされていない場合、504 Unsatisfiable Request 応答が返されます
+        // reload: キャッシュを使用せずに、常にサーバーからレスポンスを取得します。
         cache: 'no-cache'
-    });
+    })
 
-    if (!res.ok) {
-        throw new Error('Something went wrong');
-    }
+    if(!res.ok) throw new Error(res.statusText)
 
-    return await res.json();
+    const notes: Note[] = await res.json()
+    return notes
 }
 
-
-export default function NotesList() {
-    const {
-        data,
-        error,
-        isLoading
-    } = useSWR<Note[]>(
-        `${process.env.NEXT_PUBLIC_URL}/rest/v1/notes?select=*`,
-        fetcher
-    );
-
-    if (error) {
-        return "An error has occurred.";
-    }
-    if (isLoading) {
-        return <Loading/>;
-    }
+export default async function NotesList() {
+    const data = await fetchNotes()
 
     return (
-        <div>
-            <p className={"my-4 text-xl font-medium underline underline-offset-4"}>Notes</p>
-
-            <ul className={"m-3"}>
-                {data?.map((note: Note) => (
-                    <li key={note.id}>
-                        <p>{note.title}</p>
+        <>
+            <p className="my-4 pb-3 text-xl font-medium underline underline-offset-4">Notes</p>
+            <ul className="m-3">
+                {data?.map(note => (
+                    <li key={note.id} className="mb-2">
+                        <p className="text-lg font-medium">{note.title}</p>
                         <p>
-                            <strong className={"mr-3"}>
-                                Created at
+                            <strong className="mr-3">
+                                Created at:
                             </strong>
-                            {note && format(new Date(note.created_at), 'yyyy-MM-dd HH:mm:ss')}
+                            {note && format(new Date(note.created_at), 'yyyy-mm-dd')}
                         </p>
                     </li>
                 ))}
             </ul>
-        </div>
+        </>
     )
 }
-
